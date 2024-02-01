2be2765cb4866672d00fd3fb7ef5ecf94157b20a
Alexey Burak <byraklesha@gmail.com>
2024-02-01 12:37:22 +0300

    feat: add utility class annotation

--- driver-service/src/main/java/com/modsen/cabaggregator/driverservice/util/Constants.java
+++ driver-service/src/main/java/com/modsen/cabaggregator/driverservice/util/Constants.java
@@ -1,12 +1,10 @@
 package com.modsen.cabaggregator.driverservice.util;
 
-public final class Constants {
+import lombok.experimental.UtilityClass;
 
-    private Constants() {
-    }
-
-    public static final String PHONE_REGEXP = "^(80(29|44|33|25)\\d{7})$";
-    public static final String DECIMAL_FORMAT_PATTERN = "#.#";
-    public static final int DEFAULT_SCORE = 5;
-
+@UtilityClass
+public class Constants {
+    public final String PHONE_REGEXP = "^(80(29|44|33|25)\\d{7})$";
+    public final String DECIMAL_FORMAT_PATTERN = "#.#";
+    public final int DEFAULT_SCORE = 5;
 }
