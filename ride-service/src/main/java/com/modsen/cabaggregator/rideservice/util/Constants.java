9f02e91d02d77f0cb6f3fda84e4d8c2802636ae4
Alexey Burak <byraklesha@gmail.com>
2024-02-08 21:35:01 +0300

    refactor: add mapping constants for ride service

--- ride-service/src/main/java/com/modsen/cabaggregator/rideservice/util/Constants.java
+++ ride-service/src/main/java/com/modsen/cabaggregator/rideservice/util/Constants.java
@@ -6,6 +6,13 @@ import lombok.experimental.UtilityClass;
 public class Constants {
     public final String PROMO_CODES_ENDPOINT = "/api/v1/promos";
     public final String RIDES_ENDPOINT = "/api/v1/rides";
+    public final String NAME_MAPPING = "/{name}";
+    public final String HISTORY_MAPPING = "/history";
+    public final String ID_MAPPING = "/{id}";
+    public final String ID_STATUS_MAPPING = "/{id}/status";
+    public final String ID_REJECT_MAPPING = "/{id}/reject";
+    public final String ID_START_MAPPING = "/{id}/start";
+    public final String ID_FINISH_MAPPING = "/{id}/finish";
     public final String PROMO_CODES = "Promo codes";
     public final String RIDES = "Rides";
 }
